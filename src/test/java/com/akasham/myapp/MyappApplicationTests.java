2806b63013355fb41232073d505edd718928adde
akashmurumkar <akashmurumkar@outlook.com>
2025-05-30 13:22:13 +0530

    First Commit 30 May 2025

---
+++ src/test/java/com/akasham/myapp/MyappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akasham.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
